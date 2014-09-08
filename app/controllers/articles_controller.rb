bfedde519b857aeae271b8cde3cbbf8d13a96fa6
Mauro Dalu <mauro@dalu.us>
2014-09-08 11:32:22 +0200

    changed forms to partials and fixed routes for destroy action and for the home page

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,45 @@
+class ArticlesController < ApplicationController
+  include ArticlesHelper
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+  
+  def create
+  	@article = Article.new(article_params)
+  	@article.save
+  	
+  	redirect_to article_path(@article)
+  end
+  
+  def destroy
+  	Article.destroy(params[:id])
+  	
+  	flash.notice = "Article '#{params[:id]}' removed!"	
+  	
+  	redirect_to action: 'index'
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    
+    flash.notice = "Article '#{@article.title}' updated!"
+    
+    redirect_to article_path(@article)
+  end
+
+
+end
